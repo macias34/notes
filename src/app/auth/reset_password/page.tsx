05b5b269ae7714e35f256041a0573ca332a00690
macias34 <propiesek@gmail.com>
2023-02-25 19:44:59 +0100

    Added ResetPassword page

---
+++ src/app/auth/reset_password/page.tsx
@@ -0,0 +1,10 @@
+import "server-only";
+import ResetPassword from "@/components/ResetPassword/ResetPassword";
+
+export const revalidate = 0;
+
+const ResetPasswordPage = () => {
+  return <ResetPassword />;
+};
+
+export default ResetPasswordPage;
